0af1246fe1e7b76420795cd5e67853219849b094
bossdaibang123 <thta2327@gmail.com>
2021-03-19 20:05:32 -0500

    updating our files

--- assets/script.js
+++ assets/script.js
@@ -6,31 +6,41 @@ dayDisplay.text(currentDay.format('dddd MMM Do YYYY, h:mm:ss'));
 
 //localStorage.clear(); 
 
-
 // make a emty arry to store time list
 var X = localStorage.getItem("localData")
 var data = JSON.parse(X) || []            
 
+
+
 // calling time list for calender 
-for ( i=9; i<18; i++) {
-    
-    if ( i>=13) {
-        var j = i - 12 
-    } else {
-        j = i
-    }
-    
-    var time = {
-        ivalue : i,
-        hour : j,
-        surfix : "AM",
-        note : ""
-    }
 
-    if ( i>11) { time.surfix = "PM"; }
+// if do have data in localstorage, use it
+if (data != null) {
 
-    data.push(time); 
+// don't have data, make a new one
+// } else {
+    for ( i=9; i<18; i++) {
+        
+        if ( i>=13) {
+            var j = i - 12 
+        } else {
+            j = i
+        }
+        
+        var time = {
+            ivalue : i,
+            hour : j,
+            surfix : "AM",
+            note : ""
+        }
+        
+        if ( i>11) { time.surfix = "PM"; }
+        
+        data.push(time);
+    }
 }
+
+localStorage.setItem("localDate", JSON.stringify(data));
 
 // make the line work with time by change the color
 function checkHour (containerInput, index) {
@@ -47,17 +57,17 @@ function checkHour (containerInput, index) {
 var container = $("#container");
 
 // container = repeat the line element by i< data.length   
-data.forEach( function (data, index){
-    // line elememt  =  time + input + button
+data.forEach(function (data, index){
+    //   divContainer =  time + input + button
     var  divContainer = $('<div class="row">'); 
 
-    // time div
+    // time
     var containerTime = $('<div>');
         containerTime.addClass('time-block hour col-1');
         containerTime.text(data.hour+':00'+' '+data.surfix);
         divContainer.append(containerTime);
         
-    // inpt div    
+    // inpt 
     var containerInput = $('<textarea>');
         containerInput.addClass('description col-10');
         containerInput.text(data.note);
@@ -65,7 +75,7 @@ data.forEach( function (data, index){
         checkHour(containerInput, index);
         divContainer.append(containerInput);        
 
-    // btn  div    
+    // button     
     var containerBtn = $('<button><i class="fas fa-save fa"></i>');
         containerBtn.addClass('saveBtn col-1');
         data.note = containerBtn.val();
@@ -74,8 +84,6 @@ data.forEach( function (data, index){
     container.append(divContainer); 
 });
 
-
-
 // work with the btn that can save and update data
 container.on('click', '.saveBtn', function () {    
 
@@ -83,15 +91,22 @@ container.on('click', '.saveBtn', function () {
     var dataEntered = $(this).siblings('textarea');
     // specified the text input by add data-idx
     var dataEnteredIndex = dataEntered.attr('data-index');
-    // push the text in dataEntered to note in array data
-    data[dataEnteredIndex].note = dataEntered.val() ;
-    
-    console.log(dataEntered.val())
-    console.log(data[dataEnteredIndex].note)
+    // check if user add text or not  
+    if (data[dataEnteredIndex].note === dataEntered.val()) {
+        return 
+    } else {
+        // push the text in dataEntered to note in array data
+        data[dataEnteredIndex].note = dataEntered.val() ;
+    }
 
-    localStorage.setItem("localData", JSON.stringify(data));
+
+    // check if there is any 
+    
+   // localStorage.setItem("localData", JSON.stringify(data[dataEnteredIndex].note));
+    localStorage.setItem("localDate", JSON.stringify(data));
 });
 
+console.log( data )
 
 
 
